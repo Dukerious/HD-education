2a4753b4b710d5679f27cb797bfc8bad4530931d
dukey <z5306254@ad.unsw.edu.au>
2023-04-11 22:01:04 +1000

    update sort

--- week9/lecture/sorting.c
+++ week9/lecture/sorting.c
@@ -14,23 +14,17 @@ void quick_sort(int a[], int lo, int hi);
 void find_median(int a[], int lo, int hi);
 int partition(int a[], int lo, int hi);
 
+void shellSort(int a[], int lo, int hi);
+
+
 bool less(int a, int b);
 void swap(int a[], int m, int n);
-/*void shellSort(int a[], int lo, int hi);
-void bubble_sort(int *a, int lo, int hi);
-void selection_sort(int *a, int lo, int hi);
-void swap(int *a, int *b);
-bool less(int a, int b);
-void medianOfThree(Item a[], int lo, int hi);
-void quicksort(int a[], int lo, int hi);
-int partition(int a[], int lo, int hi);
-void mergesort(Item a[], int lo, int hi);
-void merge(Item a[], int lo, int mid, int hi);*/
+
 int main(void) {
     int array[] = { 18, 14, 17, 19, 8, 13, 6, 8, 18, 5, 20, 13 };
-    //selection_sort(array, 0, 8);
-    quick_sort(array, 0, 11);
-    //bubble_sort(array, 0, 11);
+
+    shellSort(array, 0, 11);
+
     print_array(array, 12);
     return 0;
 }
@@ -44,6 +38,24 @@ void insertion_sort(int a[], int lo, int hi) {
             swap(a, j, j-1);
         }
     }
+}
+
+void shellSort(int a[], int lo, int hi)
+{
+   int hvals[8] = {701, 301, 132, 57, 23, 10, 4, 1};
+   int g, h, start, i, j, val;
+   for (g = 0; g < 8; g++) {
+      h = hvals[g];
+      start = lo + h;
+      for (i = start; i <= hi; i++) {
+         val = a[i];
+         for (j = i; j >= start; j -= h) {
+            if (val >= a[j-h]) break;
+            a[j] = a[j-h];
+         }
+         a[j] = val;
+      }
+   }
 }
 
 void bubble_sort(int a[], int lo, int hi) {
@@ -146,149 +158,3 @@ void print_array(int *array, int hi) {
     putchar('\n');
 }
 
-
-
-
-
-
-
-
-
-
-
-/*void insertion_sort(int *a, int lo, int hi) {
-    for(int i = lo + 1; i < hi + 1; i++) {
-        int val = a[i];
-        int j;
-        for(j = i; j > lo; j--) {
-            if(val > a[j - 1])
-                break;
-            a[j] = a[j - 1];
-        }
-        a[j] = val;
-    }
-}
-void bubble_sort(int *a, int lo, int hi) {
-    for(int i = hi; i >= lo; i--) {
-        int sorted = 1;
-        for(int j = lo; j < i; j++) {
-            if(a[j] > a[j + 1]) {
-                swap(&a[j], &a[j + 1]);
-                sorted = 0;
-            }
-        }
-        if(sorted == 1) {
-            break;
-        }
-    }
-}
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void selection_sort(int *a, int lo, int hi) {
-    for(int i = lo; i < hi; i++) {
-        for(int j = i + 1; j <= hi; j++) {
-            if(a[i] > a[j]) {
-                swap(&a[i], &a[j]);
-            }
-        }
-    }
-}
-
-void shellSort(int a[], int lo, int hi)
-{
-   int hvals[8] = {701, 301, 132, 57, 23, 10, 4, 1};
-   int g, h, start, i, j, val;
-   for (g = 0; g < 8; g++) {
-      h = hvals[g];
-      start = lo + h;
-      for (i = start+1; i <= hi; i++) {
-         val = a[i];
-         for (j = i; j >= start; j -= h) {
-            if (val >= a[j-h]) break;
-            a[j] = a[j-h];
-         }
-         a[j] = val;
-      }
-   }
-}
-void medianOfThree(Item a[], int lo, int hi) {
-    int mid = (lo+hi)/2;
-    if (less(a[mid],a[lo])) swap(&a[lo], &a[mid]);
-    if (less(a[hi],a[mid])) swap(&a[mid], &a[hi]);
-    if (less(a[mid],a[lo])) swap(&a[lo], &a[mid]);
-    // now, we have a[lo] < a[mid] < a[hi]
-    // swap a[mid] to a[lo+1] to use as pivot
-    swap(&a[mid], &a[lo+1]);
-}
-void quicksort(int a[], int lo, int hi)
-{
-   int i; // index of pivot
-   if (hi <= lo) return;
-   //medianOfThree(a, lo, hi);
-   i = partition(a, lo, hi);
-   quicksort(a, lo, i-1);
-   quicksort(a, i+1, hi);
-}
-
-int partition(int a[], int lo, int hi)
-{
-   int v = a[lo];  // pivot
-   int  i = lo+1, j = hi;
-   for (;;) {
-      while (less(a[i],v) && i < j) i++;
-      while (less(v,a[j]) && j > i) j--;
-      if (i == j) break;
-      swap(&a[i],&a[j]);
-   }
-   j = less(a[i],v) ? i : i-1;
-   swap(&a[lo],&a[j]);
-   return j;
-}
-
-
-void mergesort(Item a[], int lo, int hi)
-{
-    int mid = (lo + hi) / 2; // mid point
-    if (hi <= lo) return;
-    mergesort(a, lo, mid);
-    mergesort(a, mid+1, hi);
-    merge(a, lo, mid, hi);
-}
-
-void merge(Item a[], int lo, int mid, int hi)
-{
-    int i, j, k, nitems = hi - lo + 1;
-    Item *tmp = malloc(nitems * sizeof(Item));
-    i = lo; j = mid + 1; k = 0;
-    // scan both segments, copying to tmp
-    while (i <= mid && j <= hi) {
-        if (less(a[i], a[j]))
-        tmp[k++] = a[i++];
-        else
-        tmp[k++] = a[j++];
-    }
-    // copy items from unfinished segment
-    while (i <= mid) tmp[k++] = a[i++];
-    while (j <= hi) tmp[k++] = a[j++];
-    //copy tmp back to main array
-    for (i = lo, k = 0; i <= hi; i++, k++)
-        a[i] = tmp[k];
-    free(tmp); 
-}
-
-
-
-
-bool less(int a, int b) {
-    return(a < b);
-}
-void print_array(int *array, int hi) {
-    for (int i = 0; i < hi; i++) {
-        printf("%d ", array[i]);
-    }
-    putchar('\n');
-}*/
